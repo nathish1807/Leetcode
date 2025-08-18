7e230b4939633c839779a66727a450b9788aa54d
nathish1807 <nathishmuthukumar@gmail.com>
2025-08-18 18:10:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (66.33%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -3,7 +3,10 @@ import java.util.*;
 class TreeNode {
     int val;
     TreeNode left, right;
-    TreeNode(int x) { val = x; }
+    TreeNode(int x)
+     { 
+        val = x; 
+    }
 }
 
 class Solution {
